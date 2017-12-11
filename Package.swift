135211ae520a734e79287c460c64ba9d7c2805f7
miximka <miximka@gmail.com>
2017-12-11 20:27:00 +0100

    Add swift package file

---
+++ Package.swift
@@ -0,0 +1,11 @@
+//
+//  Package.swift
+//  MimeParser
+//
+//  Created by miximka on 08.12.17.
+//  Copyright © 2017 miximka. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(name: "MimeParser", dependencies : [], exclude: ["Tests"])
