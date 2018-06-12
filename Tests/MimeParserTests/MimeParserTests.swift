cde647a494166db46e0b43dceb180b3a66394347
Admin <admin@nextplay.ai>
2018-06-12 10:00:47 -0700

    adding test case for application attachment

--- Tests/MimeParserTests/MimeParserTests.swift
+++ Tests/MimeParserTests/MimeParserTests.swift
@@ -265,5 +265,41 @@ class ParsingMimeTests: XCTestCase {
             XCTFail("Unexpected mime content")
         }
     }
+    
+    func testCanParseEmailWithApplicationAttachment() throws {
+        let parser = MimeParser()
+        let message = TestAdditions.testResourceString(withName: "EmailWithApplicationAttachment", extension: "txt")
+        
+        // When
+        let mime = try parser.parse(message)
+        // Then
+        XCTAssertEqual(mime.header.contentType?.type, "multipart")
+        XCTAssertEqual(mime.header.contentType?.subtype, "mixed")
+        
+        if case .mixed(let mimes) = mime.content {
+            //dump(mimes)
+            XCTAssertEqual(mimes.count, 2)
+            
+            for part in mimes {
+                
+                let mimeType = part.header.contentType?.mimeType
+                let mimeSubType = part.header.contentType?.subtype
+                
+                if mimeType == .application {
+                    XCTAssertTrue(mimeSubType == "ics")
+                    let content = try part.decodedContentString()
+                    XCTAssertNotNil(content)
+                    XCTAssertTrue(content!.contains("VCALENDAR"))
+                    XCTAssertTrue(content!.contains("VEVENT"))
+                    dump(content)
+                    return
+                }
+            }
+            
+            XCTAssert(false)
+
+        }
+        
+    }
 
 }
