ee0d2176450692e1f529ae3efee1b51ab9f4f6f2
Ronald Mannak <ronaldmannak@me.com>
2022-05-23 16:36:07 -0700

    Add public initializers

---
+++ Sources/MimeParser/Mime+Initializers.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ronald Mannak on 5/23/22.
+//
+
+import Foundation
+
+public extension ContentType {
+    
+    init(type: String, subtype: String, parameters: [String: String], ignore: Bool = false) {
+        self.type = type
+        self.subtype = subtype
+        self.parameters = parameters
+    }
+}
+
+public extension ContentDisposition {
+    
+    init(type: String, parameters: [String: String], ignore: Bool = false) {
+        self.type = type
+        self.parameters = parameters
+    }
+}
+
+public extension MimeHeader {
+    
+    init(encoding: ContentTransferEncoding?, type: ContentType?, disposition: ContentDisposition?, other: [RFC822HeaderField]) {
+        self.contentTransferEncoding = encoding
+        self.contentType = type
+        self.contentDisposition = disposition
+        self.other = other
+    }
+}
