9f852def11d2055f61f159e3e24850294858c784
Ronald Mannak <ronaldmannak@me.com>
2022-06-14 20:54:04 -0700

    Add initial unit tests
    The new unit tests all fail, we’ll work towards fixing them.

--- Tests/MimeParserTests/RFC822ExportTest.swift
+++ Tests/MimeParserTests/RFC822ExportTest.swift
@@ -18,14 +18,83 @@ class RFC822ExportTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        XCTFail()
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testSimpleMessage() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "SimpleMessage")
+        // Then
+        XCTAssertEqual(message, rfc822)
+    }
+    
+    func testSimpleMessageWithBinaryAttachment() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "SimpleMessageWithBinaryAttachment")
+        // Then
+        XCTAssertEqual(message, rfc822)
+    }
+    
+    func testSimpleMessageWithBinaryAttachmentCRLN() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "SimpleMessageWithBinaryAttachmentCRLN")
+        // Then
+        XCTAssertEqual(message, rfc822)
+    }
+    
+    func testSimpleMessageWithTextAttachment() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "SimpleMessageWithTextAttachment")
+        // Then
+        XCTAssertEqual(message, rfc822)
+    }
+    
+    func testSpecialCharsBoundary() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "SpecialCharsBoundary")
+        // Then
+        XCTAssertEqual(message, rfc822)
+    }
+    
+    func testEmailWithApplicationAttachment() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "EmailWithApplicationAttachment")
+        // Then
+        XCTAssertEqual(message, rfc822)
+    }
+    
+    func testEmailWithEmptyCc() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "EmptyCc")
+        // Then
+        XCTAssertEqual(message, rfc822)
+    }
+    
+    func testMeetingRequestWithZeroLengthPart() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "MeetingRequestWithZeroLengthPart")
+        // Then
+        XCTAssertEqual(message, rfc822)
     }
 
+    func testMessageWithEnclosedMessageAsAttachment() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "MessageWithEnclosedMessageAsAttachment")
+        // Then
+        XCTAssertEqual(message, rfc822)
+    }
+    
+    func testQuotedPrintableMessage() throws {
+        // When
+        let (message, rfc822) = try fileToRFC822(resource: "QuotedPrintableMessage")
+        // Then
+        XCTAssertEqual(message, rfc822)
+    }
 
+    func fileToRFC822(resource: String) throws -> (String, String) {
+        let parser = MimeParser()
+        let message = TestAdditions.testResourceString(withName: resource, extension: "txt")
+
+        // When
+        let mime = try parser.parse(message)
+        let rfc822 = try mime.rfc822String()
+        return (message, rfc822)
+    }
 }
