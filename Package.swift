776750279ed51cc5b876013f6aad43b98f06e544
Fahad <fahad@guidedways.com>
2023-01-19 10:03:02 +0400

    Support dynamic library

--- Package.swift
+++ Package.swift
@@ -4,22 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "MimeParser",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "MimeParser",
-            targets: ["MimeParser"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target( name: "MimeParser", dependencies: []),
-        .testTarget(name: "MimeParserTests", dependencies: ["MimeParser"], resources: [.process("Resources")])
-
-    ]
+  name: "MimeParser",
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "MimeParser",
+      targets: ["MimeParser"]),
+    .library(
+      name: "MimeParserDynamic",
+      type: .dynamic,
+      targets: ["MimeParser"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target( name: "MimeParser", dependencies: []),
+    .testTarget(name: "MimeParserTests", dependencies: ["MimeParser"], resources: [.process("Resources")])
+    
+  ]
 )
